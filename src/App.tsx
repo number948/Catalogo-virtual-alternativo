2ff8c7bb22588ac6552b3db8cb64253da989f1f1
number948 <scottsykotom@gmail.com>
2022-08-26 01:09:22 -0400

    agregando más objetos

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
 import './App.css';
 import React, { Component } from 'react';
-import { Objeto } from './componentes/objeto';
+import Objeto from './componentes/objeto';
+import imagen_logo from './imagenes/logo-fundación.png'; 
+import imagen_hacha from './imagenes/bombero-hacha.png';
+import imagen_traje from './imagenes/bombero-traje.png';
+import imagen_casco from './imagenes/bombero-casco.png';
+import imagen_casco_2 from './imagenes/bombero-casco-2.png';
 
-function App() {
+function App ()   {
   return (
     <div className="App">
+        <div className=' contenedor-titulo'>
+        
+            <h1>Fundación Cultural y Deportiva de Bomberos de Chile (FCDBCH)</h1> 
+            
+            <h2> Museo virtual<img className='imagen-logo-fundacion' src= {imagen_logo} alt='imagen-logo' /> </h2>
+            
+        </div>
      <div className='contenedor-principal'>
-    <h2> Museo virtual </h2>
-    <h1>Fundación Cultural y Deportiva de Bomberos de Chile (FCDBCH)</h1>
-    
-    <Objeto />
-     </div>
-    </div>
+        <Objeto 
+            image= {imagen_traje}
+            nombre = 'Uniforme Bombero'
+            localidad = 'Región del Maule'
+            uso = 'Vestuario'
+          />
+
+        <Objeto
+          image= {imagen_casco}
+          nombre = 'Casco de Bombero'
+          localidad = 'Región metropolitana'
+          uso = 'Vestuario'
+        />
+
+        <Objeto
+          image= {imagen_hacha}
+          nombre = 'Hacha de Bombero'
+          localidad = 'Región de Valparaíso'
+          uso = 'Herramienta'
+        />
+
+        <Objeto
+          image= {imagen_casco_2}
+          nombre = 'Casco de Bombero'
+          localidad = 'Región de Los Lagos'
+          uso = 'Vestuario'
+        />
+
+
+        </div>
+      </div>
   );
 }
 
--- src/componentes/objeto.tsx
+++ src/componentes/objeto.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import App from "../App";
-import imagenLogoAUsar from '../imagenes/bombero-traje.png';
 
-export const Objeto = () => {
+
+
+interface ObjetoProps {
+   image: string;
+   nombre: string;
+   localidad :string;
+   uso :string;
+
+}
+
+function Objeto (props: ObjetoProps)   {
   return (
     <div className='contenedor-objeto'>
       <img
         className="imagen-objeto"
-        src = {imagenLogoAUsar}
-        alt= 'imagen-traje-bombero'/>
+        src = {props.image}
+        alt= 'imagen-bombero'/>
+      
       <div className='contenedor-descripcion-objeto'>
-        <p className='nombre-objeto'> Uniforme de Bombero ignífugo </p>
-        <p className='localidad-objeto'> Región de Valparaíso</p>
-        <p className='uso-objeto'>Vestuario </p>
-    
-        </div>  
+        <p className='nombre-objeto'>{props.nombre}</p>
+        <p className='localidad-objeto'>{props.localidad}</p>
+        <p className='uso-objeto'>{props.uso}</p>
+        </div>   
     </div>
   );
 };
